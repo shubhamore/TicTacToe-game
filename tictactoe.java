75a739a870a1529b1f581f8044a288be155e5330
shubham <shubhamore02@gmail.com>
2022-09-18 16:38:38 +0530

    added welcome message

--- tictactoe.java
+++ tictactoe.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class tictactoe {
     public static void main(String[] args) {
+        System.out.println("welcome to the game user.");
         Scanner sc=new Scanner(System.in);
         String player1="shubham";
         String player2="devang";
